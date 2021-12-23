cc33bcfd3168f2f95072b3fe8484b1ebe60dfcba
Oleksandr Arseniuk <korol1511@gmail.com>
2021-12-23 20:27:22 +0200

    Test react

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="profile">
+      <div class="description">
+        <img
+          // src={avatar}
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">Petra Marica</p>
+        <p class="tag">@pmarica</p>
+        <p class="location">Salvador, Brasil</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
     </div>
   );
 }

6b84fefdf74c17d42c3bd3e91563daa8c46cbd0e
Oleksandr Arseniuk <korol1511@gmail.com>
2021-12-28 22:18:02 +0200

    Тест 2 для работы

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,54 @@
-// import props from "../user.json";
-import UserCards from './Profile.styled';
+import props from "../user.json";
+import { UserCards, Description, Avatar, Name, Tag, Location, Stats, Label, Quantity } from './Profile.styled';
+import { MdAlternateEmail } from 'react-icons/Md'
 
 function UserCard() {
   return (
     <UserCards>
-sdfsdf
+      <Description>
+        <Avatar src={props.avatar}>
+        </Avatar>
+        <Name>{ props.username }</Name>
+        <Tag><MdAlternateEmail />{ props.tag }</Tag>
+        <Location>{ props.location }</Location>
+      </Description>
+      <Stats>
+        <Label>Folowers</Label>
+        <Quantity>{props.stats.followers}</Quantity>
+        <Label>Views</Label>
+        <Quantity>{props.stats.views }</Quantity>
+        <Label>Likes</Label>
+        <Quantity>{ props.stats.likes }</Quantity>
+      </Stats>
     </UserCards>
   );
 }
 export default UserCard;
+
+/* <div class="profile">
+  <div class="description">
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      alt="User avatar"
+      class="avatar"
+    />
+    <p class="name">Petra Marica</p>
+    <p class="tag">@pmarica</p>
+    <p class="location">Salvador, Brasil</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">1000</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">2000</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">3000</span>
+    </li>
+  </ul>
+</div> */
--- src/components/Profile.styled.jsx
+++ src/components/Profile.styled.jsx
@@ -1,6 +1,58 @@
 import styled from 'styled-components';
 
 export const UserCards = styled.div`
-    position: relative;
-    background-color: blue;
+    width: 1000px;
+    height: 1000px;
+    background-color: grey;
 `;
+
+export const Description = styled.div`
+    position: relative;
+    width: 400px;
+    height: 500px;
+    margin: auto;
+    top: 250px;
+    /* margin-top: ; */
+    background-color: white;
+    border-radius: 2%;
+
+`;
+
+export const Avatar = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 50px;
+    margin-top: 50px;
+    margin-left: 150px;
+    background-color: white;
+`;
+
+export const Name = styled.p`
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 700;
+    font-size: 25px;
+    text-align: center;
+`;
+
+export const Tag = styled.p`
+    text-align: center;
+    
+
+`;
+
+export const Location = styled.p`
+    text-align: center;
+`;
+
+export const Stats = styled.ul`
+
+`;
+
+export const Label = styled.span`
+
+`;
+
+export const Quantity = styled.span`
+
+`;
+
